72627141dd038ae98f4ea1c399b795c4235b3619
Alisson Marcos <alissonmarcos249@gmail.com>
2024-05-17 20:41:19 -0300

    add spaces between colors

--- philo/src/logger.c
+++ philo/src/logger.c
@@ -7,15 +7,15 @@ void	*logger(void *param)
 
 	philo = (t_philo *) param;
 	if (philo->state == TAKEN_FORK)
-		string = CYAN"has taken a fork"RESET"\n";
+		string = CYAN "has taken a fork" RESET "\n";
 	else if (philo->state == EATING)
-		string = WHITE"is eating"RESET"\n";
+		string = WHITE "is eating" RESET "\n";
 	else if (philo->state == SLEEPING)
-		string = BLUE"is sleeping"RESET"\n";
+		string = BLUE "is sleeping" RESET "\n";
 	else if (philo->state == THINKING)
-		string = YELLOW"is thinking"RESET"\n";
+		string = YELLOW "is thinking" RESET "\n";
 	else if (philo->state == DIED)
-		string = RED"died"RESET"\n";
+		string = RED "died" RESET "\n";
 	else
 		return (NULL);
 	write(1, string, ft_strlen(string));
@@ -24,7 +24,7 @@ void	*logger(void *param)
 
 int	ft_strlen(char *s)
 {
-	char *start;
+	char*start;
 
 	start = s;
 	while (*s)
