6aaf8c27e98e6dbdae09065144f943097e6af01a
Alisson Marcos <alissonmarcos249@gmail.com>
2024-05-17 20:25:47 -0300

    add colors

--- philo/include/philo.h
+++ philo/include/philo.h
@@ -4,6 +4,16 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <stdbool.h>
+
+# define YELLOW "\001\033[1;33m\002"
+# define GREEN "\001\033[1;32m\002"
+# define RED "\001\033[1;31m\002"
+# define BLUE "\001\033[1;94m\002"
+# define PURPLE "\001\033[1;35m\002"
+# define CYAN "\001\033[1;36m\002"
+# define WHITE "\001\033[1;37m\002"
+# define RESET "\001\033[0m\002"
 
 typedef struct s_data t_data;
 typedef struct s_fork t_fork;
@@ -30,6 +40,7 @@ struct s_data
 struct s_philo
 {
 	int				id;
+	bool			action_done;
 	t_states		state;
 	t_data			*data;
 };
--- philo/src/main.c
+++ philo/src/main.c
@@ -4,7 +4,12 @@ int main(void)
 {
 	pthread_t t1;
 
+	pthread_create(&t1, NULL, logger, &(t_philo) {.state = THINKING});
 	pthread_create(&t1, NULL, logger, &(t_philo) {.state = SLEEPING});
+	pthread_create(&t1, NULL, logger, &(t_philo) {.state = DIED});
+	pthread_create(&t1, NULL, logger, &(t_philo) {.state = EATING});
+	pthread_create(&t1, NULL, logger, &(t_philo) {.state = TAKEN_FORK});
 	pthread_join(t1, NULL);
+
 	return 0;
 }
